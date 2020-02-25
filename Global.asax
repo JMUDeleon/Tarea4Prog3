8ce0a46907522e357929013104b78c2608e0d17f
Johan <Johanmanuel13579@gmail.com>
2020-02-25 16:43:39 -0400

    Tarea4Prog3

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tarea4Pr3.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tarea4Pr3.Startup))]
+namespace tarea4Pr3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
